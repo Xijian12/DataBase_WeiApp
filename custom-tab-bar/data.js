29a3d1cfe55242fe901a68e8f98e96d1e1c22ec0
JianXi <3146624068@qq.com>
2024-04-19 13:44:48 +0800

    初次修改

---
+++ custom-tab-bar/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    icon: 'home',
+    text: '首页',
+    url: 'pages/home/home',
+  },
+  {
+    icon: 'sort',
+    text: '维修零件',
+    url: 'pages/goods/category/index',
+  },
+  {
+    icon: 'person',
+    text: '个人中心',
+    url: 'pages/usercenter/index',
+  },
+];
